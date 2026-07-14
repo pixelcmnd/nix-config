e5b6b1858637f02b48e4ce00d441cfdcd126aa4d
Daniil Bright <pixelcmnd@icloud.com>
2026-07-14 11:22:50 +0300

    chore: workaround whisper-cpp build issue

--- home/core/core.nix
+++ home/core/core.nix
@@ -37,7 +37,7 @@
     imagemagick # Software suite to create, edit, compose, or convert bitmap images
     btop # Monitor of resources
     just # Handy way to save and run project-specific commands
-    ffmpeg-full # Complete, cross-platform solution to record, convert and stream audio and video
+    ffmpeg # Complete, cross-platform solution to record, convert and stream audio and video
     qemu # Generic and open source machine emulator and virtualizer
   ];
 
