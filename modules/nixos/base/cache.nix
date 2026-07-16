e676bcb0f162ba4bf5af697170eb2a6b63a914b3
Daniil Bright <pixelcmnd@icloud.com>
2026-07-16 19:17:50 +0300

    refactor: remove unused binary cache provider

--- modules/nixos/base/cache.nix
+++ modules/nixos/base/cache.nix
@@ -13,14 +13,10 @@
 
     # substituers that will be considered before the official ones(https://cache.nixos.org)
     substituters = [
-      "https://cache.garnix.io"
-      "https://install.determinate.systems"
       "https://nix-community.cachix.org"
     ];
 
     trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     builders-use-substitutes = true;
