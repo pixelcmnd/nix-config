5f88cdf2b2f40942fc499f636d846769e1b6de47
Daniil Bright <pixelcmnd@icloud.com>
2026-09-12 18:44:48 +0300

    pkgs: remove unused packages

--- home/core/dev.nix
+++ home/core/dev.nix
@@ -2,11 +2,7 @@
   # dev utils / compilers / programming langs etc.
   home.packages = with pkgs; [
     # nix related
-    hydra-check # check hydra(nix's build farm) for the build status of a package
-    nix-index # A small utility to index nix store paths
-    nix-init # generate nix derivation from url
     nix-melt # A TUI flake.lock viewer https://github.com/nix-community/nix-melt
-    nix-tree # A TUI to visualize the dependency graph of a nix derivation https://github.com/utdemir/nix-tree
     deadnix # Find and remove unused code in .nix source files
     nixd # Feature-rich Nix language server interoperating with C++ nix
     alejandra # Nix formatter
